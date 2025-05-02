c05ed2de08e4fc69482ea520a9b416d6cf319128
Mykhailo Hakman <deorfeal.it@gmail.com>
2025-05-02 13:04:08 +0200

    Add basic layout components - heading

---
+++ src/components/pieces/Heading.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="heading">
+    <div class="container">
+      <div class="heading__inner">
+        <h1 class="heading__title">
+          <slot></slot>
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.heading {
+  padding-top: 100px;
+  margin-bottom: 50px;
+
+  &__inner {
+    display: grid;
+    grid-template-columns: 1fr 388px;
+    align-items: center;
+    gap: 32px;
+  }
+
+  &__title {
+    color: #FFF;
+    font-size: 128px;
+    font-weight: 700;
+    line-height: 136px;
+    letter-spacing: -4px;
+  }
+}
+
+@media (max-width: 1200px) {
+  .heading__title {
+    font-size: 96px;
+    line-height: 108px;
+    letter-spacing: -2px;
+  }
+}
+
+@media (max-width: 800px) {
+  .heading__title {
+    font-size: 74px;
+    line-height: 78px;
+    letter-spacing: -1px;
+  }
+}
+
+@media (max-width: 550px) {
+  .heading__title {
+    font-size: 56px;
+    line-height: 68px;
+  }
+
+  @media (max-width: 550px) {
+    .heading {
+      padding-top: 50px;
+      margin-bottom: 25px;
+    }
+  }
+}
+</style>
