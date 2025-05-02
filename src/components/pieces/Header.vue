e75754c16f5b0a1472e569240b39d82986d95e35
Mykhailo Hakman <deorfeal.it@gmail.com>
2025-05-02 13:02:24 +0200

    Add basic layout components - header

---
+++ src/components/pieces/Header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <a class="header__logo logo" href="#">
+                    <img class="logo__img" src="/images/logo.svg" alt="logo">
+                </a>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+.header {
+    padding: 25px 0;
+    border-bottom: 1px solid #4B515A;
+
+    &__inner {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 30px;
+    }
+}
+
+.logo {
+    display: block;
+    width: 85px;
+    height: 19px;
+
+    &__img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+}
+</style>
