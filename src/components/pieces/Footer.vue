e8cc2f33f8acf5507dc0c308089632876ba6996e
Mykhailo Hakman <deorfeal.it@gmail.com>
2025-05-02 12:59:06 +0200

    Add basic layout components - footer

---
+++ src/components/pieces/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <div class="footer__inner">
+                <p class="footer__copy">
+                    Workout © 2024. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.footer {
+    border-top: 1px solid #4B515A;
+    &__inner {
+        padding: 30px 0;
+    }
+
+    &__copy {
+        color: #FFF;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 20px;
+    }
+}
+
+@media (max-width: 550px) {
+    .footer__inner {
+        padding-top: 25px;
+        padding-block: 25px;
+    }
+    .footer__copy {
+        font-size: 12px;
+        line-height: 16px;
+    }
+}
+</style>
